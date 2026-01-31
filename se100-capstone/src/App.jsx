20540b60a575d79b1d076ed5fe2792da16bf801a
clarissa-lyl <chaosandriots@gmail.com>
2026-02-01 00:20:53 +0800

    Add files for Assignment 7

---
+++ se100-capstone/src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import StockForm from './StockForm.jsx'
+
+//import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Finance Dashboard</h1>
+    <StockForm />
+
+    </>
+  )
+}
+
+export default App
