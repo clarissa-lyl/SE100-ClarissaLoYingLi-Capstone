660373ffb4a0bf56e319b37838618ff9a37ccfed
clarissa-lyl <chaosandriots@gmail.com>
2026-02-07 11:30:48 +0800

    Clean up macOS artifacts and update Vite config

--- se100-capstone/vite.config.js
+++ se100-capstone/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/se100-clarissaloyingli-capstone/se100-capstone/',
 })
